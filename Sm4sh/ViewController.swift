b6bf978668b5eb52ef202e27af0ff47631ab1b95
Jose Javier Pabon <pabonjose87@gmail.com>
2018-12-01 15:53:38 -0500

    Initial Commit

---
+++ Sm4sh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sm4sh
+//
+//  Created by José Javier on 12/1/18.
+//  Copyright © 2018 José Javier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
